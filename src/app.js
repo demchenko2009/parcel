66f3e3bf882ad404bfb979b49380c647b40dacf0
demchenko2009 <166243357+demchenko2009@users.noreply.github.com>
2025-10-11 14:34:06 +0300

    ffff

--- src/app.js
+++ src/app.js
@@ -28,3 +28,30 @@ const result4 = calkulator.minus(11,6)
 console.log(result3);
 console.log(result4);
 
+
+
+
+
+
+
+
+
+
+
+import { nanoid } from "nanoid";
+
+const people = [
+  { name: "iса", lastName: "Петренко", age: 25 },
+  { name: "Олена", lastName: "Ковальчук", age: 32 },
+  { name: "Максим", lastName: "Сидоренко", age: 28 },
+  { name: "Аліна", lastName: "Ткаченко", age: 22 },
+  { name: "Юрій", lastName: "Мельник", age: 35 },
+  { name: "Наталя", lastName: "Іванова", age: 30 },
+  { name: "Дмитро", lastName: "Бондар", age: 27 },
+  { name: "Світлана", lastName: "Кравченко", age: 29 },
+  { name: "Артем", lastName: "Гончар", age: 31 },
+  { name: "Марина", lastName: "Литвин", age: 26 }
+];
+
+const formatId = people.map(user => ({...user, id : nanoid()}))
+console.log(formatId);
