13a06cf49663e3cfc264cd84b10344cb3c45beae
demchenko2009 <166243357+demchenko2009@users.noreply.github.com>
2025-10-11 13:57:15 +0300

    fff

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
-console.log("hello leon");
+
+
+// default import
+// import added from "./plus"
+
+// const result = added(5,6)
+//  console.log(result);
+ 
+
+
+// imenovan import
+
+
+// import {add,minus} from "./plus"
+
+// const result = add (6,9)
+// console.log(result);
+
+// const result2 = minus(7,4)
+// console.log(result2);
+
+import calkulator from "./plus"
+console.log(calkulator);
+
+
+const result3 = calkulator.add(11,6)
+const result4 = calkulator.minus(11,6)
+console.log(result3);
+console.log(result4);
+
---
+++ src/plus.js
@@ -0,0 +1,32 @@
+
+// exsport defalt може експортувати щось одне 
+// export default  function add (a,b) {
+//     return a + b
+// }
+
+
+
+// imenovan exsport
+
+// export function add(a,b) {
+//     return a + b 
+// }
+
+// export function minus(a,b) {
+//     return a - b 
+// }
+
+
+
+
+ function add(a,b) {
+    return a + b 
+}
+
+ function minus(a,b) {
+    return a - b 
+}
+
+
+
+export default {add,minus}
