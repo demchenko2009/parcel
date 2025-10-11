043dada5142bae4fbcb1f200d5d23d152a5bc5a4
demchenko2009 <166243357+demchenko2009@users.noreply.github.com>
2025-10-11 14:33:19 +0300

    Updates

--- parcel.0f77c784.js
+++ parcel.0f77c784.js
@@ -714,8 +714,177 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 }
 
 },{}],"2R06K":[function(require,module,exports,__globalThis) {
-console.log("hello leon");
+// default import
+// import added from "./plus"
+// const result = added(5,6)
+//  console.log(result);
+// imenovan import
+// import {add,minus} from "./plus"
+// const result = add (6,9)
+// console.log(result);
+// const result2 = minus(7,4)
+// console.log(result2);
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+var _plus = require("./plus");
+var _plusDefault = parcelHelpers.interopDefault(_plus);
+var _nanoid = require("nanoid");
+console.log((0, _plusDefault.default));
+const result3 = (0, _plusDefault.default).add(11, 6);
+const result4 = (0, _plusDefault.default).minus(11, 6);
+console.log(result3);
+console.log(result4);
+const people = [
+    {
+        name: "i\u0441\u0430",
+        lastName: "\u041F\u0435\u0442\u0440\u0435\u043D\u043A\u043E",
+        age: 25
+    },
+    {
+        name: "\u041E\u043B\u0435\u043D\u0430",
+        lastName: "\u041A\u043E\u0432\u0430\u043B\u044C\u0447\u0443\u043A",
+        age: 32
+    },
+    {
+        name: "\u041C\u0430\u043A\u0441\u0438\u043C",
+        lastName: "\u0421\u0438\u0434\u043E\u0440\u0435\u043D\u043A\u043E",
+        age: 28
+    },
+    {
+        name: "\u0410\u043B\u0456\u043D\u0430",
+        lastName: "\u0422\u043A\u0430\u0447\u0435\u043D\u043A\u043E",
+        age: 22
+    },
+    {
+        name: "\u042E\u0440\u0456\u0439",
+        lastName: "\u041C\u0435\u043B\u044C\u043D\u0438\u043A",
+        age: 35
+    },
+    {
+        name: "\u041D\u0430\u0442\u0430\u043B\u044F",
+        lastName: "\u0406\u0432\u0430\u043D\u043E\u0432\u0430",
+        age: 30
+    },
+    {
+        name: "\u0414\u043C\u0438\u0442\u0440\u043E",
+        lastName: "\u0411\u043E\u043D\u0434\u0430\u0440",
+        age: 27
+    },
+    {
+        name: "\u0421\u0432\u0456\u0442\u043B\u0430\u043D\u0430",
+        lastName: "\u041A\u0440\u0430\u0432\u0447\u0435\u043D\u043A\u043E",
+        age: 29
+    },
+    {
+        name: "\u0410\u0440\u0442\u0435\u043C",
+        lastName: "\u0413\u043E\u043D\u0447\u0430\u0440",
+        age: 31
+    },
+    {
+        name: "\u041C\u0430\u0440\u0438\u043D\u0430",
+        lastName: "\u041B\u0438\u0442\u0432\u0438\u043D",
+        age: 26
+    }
+];
+const formatId = people.map((user)=>({
+        ...user,
+        id: (0, _nanoid.nanoid)()
+    }));
+console.log(formatId);
 
-},{}]},["7wZbQ","2R06K"], "2R06K", "parcelRequire5b12", {})
+},{"./plus":"kpkBT","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT","nanoid":"328Fw"}],"kpkBT":[function(require,module,exports,__globalThis) {
+// exsport defalt може експортувати щось одне 
+// export default  function add (a,b) {
+//     return a + b
+// }
+// imenovan exsport
+// export function add(a,b) {
+//     return a + b 
+// }
+// export function minus(a,b) {
+//     return a - b 
+// }
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+function add(a, b) {
+    return a + b;
+}
+function minus(a, b) {
+    return a - b;
+}
+exports.default = {
+    add,
+    minus
+};
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}],"jnFvT":[function(require,module,exports,__globalThis) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, '__esModule', {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === 'default' || key === '__esModule' || Object.prototype.hasOwnProperty.call(dest, key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
+
+},{}],"328Fw":[function(require,module,exports,__globalThis) {
+/* @ts-self-types="./index.d.ts" */ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "urlAlphabet", ()=>(0, _indexJs.urlAlphabet));
+parcelHelpers.export(exports, "random", ()=>random);
+parcelHelpers.export(exports, "customRandom", ()=>customRandom);
+parcelHelpers.export(exports, "customAlphabet", ()=>customAlphabet);
+parcelHelpers.export(exports, "nanoid", ()=>nanoid);
+var _indexJs = require("./url-alphabet/index.js");
+let random = (bytes)=>crypto.getRandomValues(new Uint8Array(bytes));
+let customRandom = (alphabet, defaultSize, getRandom)=>{
+    let mask = (2 << Math.log2(alphabet.length - 1)) - 1;
+    let step = -~(1.6 * mask * defaultSize / alphabet.length);
+    return (size = defaultSize)=>{
+        let id = '';
+        while(true){
+            let bytes = getRandom(step);
+            let j = step | 0;
+            while(j--){
+                id += alphabet[bytes[j] & mask] || '';
+                if (id.length >= size) return id;
+            }
+        }
+    };
+};
+let customAlphabet = (alphabet, size = 21)=>customRandom(alphabet, size | 0, random);
+let nanoid = (size = 21)=>{
+    let id = '';
+    let bytes = crypto.getRandomValues(new Uint8Array(size |= 0));
+    while(size--)id += (0, _indexJs.urlAlphabet)[bytes[size] & 63];
+    return id;
+};
+
+},{"./url-alphabet/index.js":"29KoN","@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}],"29KoN":[function(require,module,exports,__globalThis) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "urlAlphabet", ()=>urlAlphabet);
+const urlAlphabet = 'useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict';
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"jnFvT"}]},["7wZbQ","2R06K"], "2R06K", "parcelRequire5b12", {})
 
 //# sourceMappingURL=parcel.0f77c784.js.map
